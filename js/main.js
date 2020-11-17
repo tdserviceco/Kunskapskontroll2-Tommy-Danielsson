858672d3dd60c540070ec7031a5257ccb4a536c3
Tommy Danielsson <tdserviceco@gmail.com>
2020-11-17 22:04:26 +0100

    V2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+const values = {
+  loading: document.querySelector('.loading'),
+  result: document.querySelector('.result'),
+  error: document.querySelector('.error'),
+  cityCompareBlock: document.querySelector('.city-compare'),
+  descriptionBlock: document.querySelector('.description'),
+  humidityBlock: document.querySelector('.humidity'),
+  degreeBlock: document.querySelector('.degree'),
+  windSpeedBlock: document.querySelector('.wind-speed'),
+}
+
 setup = () => {
-  //Vi börjar med aktivera fetchCityValue funktion
+  // Lite upplägg av variabel som ger klass till de som behöver just när sidan startar.. i detta fall class namn: hide
+  const { loading, result, error, cityCompareBlock } = values;
+  loading.classList.add('hide');
+  result.classList.add('hide');
+  error.classList.add('hide');
+  cityCompareBlock.classList.add('hide');
   fetchCityValue();
 }
 
@@ -9,59 +25,55 @@ fetchCityValue = (city) => {
   så därför säger jag till den gå till FormField och kolla där om det finns nåt.
   Får vi nåt värde tillbaka så kör vi fetchCityWeather funktionen (som är en async/await)
   */
-
   if (city === undefined) {
     return formField();
   }
   else {
+    const { loading, result, error, cityCompareBlock } = values;
+
     return fetchCityWeather(city).then(res => {
-
-      const result = document.querySelector('.result');
-      const error = document.querySelector('.error');
-      const loading = document.querySelector('.loading');
-      const descriptionBlock = document.querySelector('.description');
-      const humidityBlock = document.querySelector('.humidity');
-      const degreeBlock = document.querySelector('.degree')
-      const windSpeedBlock = document.querySelector('.wind-speed');
-      const cityCompareBlock = document.querySelector('.city-compare');
-
-      loading.classList.remove('hide');
       if (res.code === '404') {
-        loading.classList.add('hide');
-        result.classList.remove('hide');
-        result.classList.add('hide');
         error.classList.remove('hide');
-        error.classList.add('show');
-        cityCompareBlock.classList.add('hide');
+        error.classList.add('show')
         return error.children[0].textContent = res.message;
       }
       else {
-        loading.classList.add('hide');
-        error.classList.remove('show');
-        error.classList.add('hide');
-        result.classList.remove('hide');
-        result.classList.add('show');
+        error.classList.remove('show')
+        error.classList.add('hide')
+        loading.classList.add('show');
+        loading.ontransitionend = () => {
+          loading.classList.remove('show');
+          loading.classList.add('hide');
+          result.classList.add('show');
+        }
+        result.classList.remove('show');
+        result.ontransitionend = () => {
+          result.classList.add('show');
+        }
 
-        buildDescription(res.description, res.icon, descriptionBlock);
-        buildHumidity(res.humidity, humidityBlock)
-        buildDegree(res.degree, degreeBlock);
-        buildWindSpeed(res.windSpeed, windSpeedBlock);
+        buildDescription(res.description, res.icon);
+        buildHumidity(res.humidity)
+        buildDegree(res.degree);
+        buildWindSpeed(res.windSpeed);
         buildCityCompare(res).then(compare => {
+
           // Har ingen catch i denna för vi kommer alltid få rätt stad (malmö)
           // Däremot använder jag Math.abs() för omvandla - till + för vi måste säga att det är + skillnad.
           cityCompareBlock.classList.remove('hide');
           cityCompareBlock.classList.add('show');
 
-          const city = compare.compare_degree_city;
-          const malmo = compare.compare_degree_city;
-          const differenceBetweenCity = Math.abs(city - malmo);
+
+          let city = compare.compare_degree_city;
+          let malmo = compare.compare_degree_malmo;
+
+          const differenceBetweenCity = Math.abs(Math.round(malmo) - Math.round(city));
           if (compare.city === 'Malmo') {
             return cityCompareBlock.children[0].textContent = 'Jämför du samma stad?'
           }
-
           return cityCompareBlock.children[0].textContent = `Skilland mellan Malmö och ${compare.city} är: ${differenceBetweenCity} i grader`
         })
       }
+
     }).catch(issues => {
       console.error(issues);
     });
@@ -70,7 +82,7 @@ fetchCityValue = (city) => {
 
 fetchCityWeather = async (city) => {
   // Api nyckel skrivs in här
-  const apiKey = '';
+  const apiKey = '212514b52ee74f93d002ad15b350fc09';
   // Hämtar data från openweathermap.org api och ifall det är ingen stad vid namn av X då returnerar vi en error tillbaka.
   // Annars sparar vi ner saker vi vill ha och retunerar tillbaka.
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=se&units=metric&appid=${apiKey}`;
@@ -107,9 +119,12 @@ formSubmited = (e) => {
   return fetchCityValue(city);
 }
 
+//Här börjar vi bygga varje sektion hur allt ska vara
 
-buildDescription = (description, icon, descriptionBlock) => {
+buildDescription = (description, icon) => {
+  const { descriptionBlock } = values;
   // Struktur hur vi vill text och värde ska uppföra sig
+
   descriptionBlock.children[0].textContent = 'Väder:';
   descriptionBlock.children[1].children[0].textContent = description;
   descriptionBlock.children[1].children[1].setAttribute('src', icon);
@@ -117,16 +132,18 @@ buildDescription = (description, icon, descriptionBlock) => {
 }
 
 
-buildHumidity = (humidity, humidityBlock) => {
+buildHumidity = (humidity) => {
   // Struktur hur vi vill text och värde ska uppföra sig
+  const { humidityBlock } = values;
   humidityBlock.children[0].textContent = 'Fuktighet:';
   humidityBlock.children[1].textContent = `${humidity}%`;
   return humidityBlock;
 }
 
 
-buildDegree = (degree, degreeBlock) => {
+buildDegree = (degree) => {
   // Struktur hur vi vill text och värde ska uppföra sig
+  const { degreeBlock } = values;
   weatherClassAdd(degree, degreeBlock);
   degree = `${degree}°c`;
   degreeBlock.children[0].textContent = 'Temperatur:';
@@ -134,8 +151,9 @@ buildDegree = (degree, degreeBlock) => {
   return degreeBlock;
 }
 
-buildWindSpeed = (windSpeed, windSpeedBlock) => {
+buildWindSpeed = (windSpeed) => {
   // Struktur hur vi vill text och värde ska uppföra sig
+  const { windSpeedBlock } = values;
   windSpeedBlock.children[0].textContent = 'Vindhastighet:'
   windSpeedBlock.children[1].textContent = `${windSpeed} kmph`;
   return windSpeedBlock;
@@ -144,13 +162,13 @@ buildWindSpeed = (windSpeed, windSpeedBlock) => {
 weatherClassAdd = (weatherDegree, degreeBlock) => {
   // Väder check så ifall det är viss temperatur ute då byter vi färg på texten
   if (weatherDegree >= 20) {
-    return degreeBlock.children[0].classList.add('warm-weather');
+    return degreeBlock.children[1].classList.add('warm-weather');
   }
   else if (weatherDegree <= 10) {
-    return degreeBlock.children[0].classList.add('cold-weather');
+    return degreeBlock.children[1].classList.add('cold-weather');
   }
   else {
-    return degreeBlock.children[0].classList.add('decent-weather');
+    return degreeBlock.children[1].classList.add('decent-weather');
   }
 }
 
@@ -161,6 +179,7 @@ buildCityCompare = async (city) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=malmö&lang=sv&units=metric&appid=212514b52ee74f93d002ad15b350fc09`;
   let response = await fetch(url);
   let convertJson = await response.json();
+
   return {
     city: city.city_name,
     compare_degree_malmo: convertJson.main.temp,
